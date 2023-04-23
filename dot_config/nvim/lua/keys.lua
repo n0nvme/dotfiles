61fb81e1f6460231306964ab9df9e02a73ed26f1
n0nvme <svmpl3nvm3@gmail.com>
2023-04-24 01:28:55 +0300

    update telescope keymap

--- dot_config/nvim/lua/keys.lua
+++ dot_config/nvim/lua/keys.lua
@@ -1,8 +1,14 @@
-local builtin = require('telescope.builtin')
+vim.g.mapleader = " "
 
+-- nvim-tree
 vim.keymap.set('n', '<C-e>', function() 
     require('nvim-tree').focus()
 end)
 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+-- telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
