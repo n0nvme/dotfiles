435ceb28837760b1db474343ead83ac4ce92c647
n0nvme <svmpl3nvm3@gmail.com>
2024-05-19 01:52:29 +0300

    [nvim] add hardtime

--- dot_config/nvim/lua/n0nvme/plugins/init.lua
+++ dot_config/nvim/lua/n0nvme/plugins/init.lua
@@ -45,6 +45,13 @@ return {
       require("gitsigns").setup()
     end,
   },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {
+      restriction_mode = "hint", -- block or hint
+    },
+  },
   "gleam-lang/gleam.vim",
   "dstein64/vim-startuptime",
 }
