45cb7b32d17fbe42c7a28ae6396ccc99ae036504
n0nvme <svmpl3nvm3@gmail.com>
2024-05-19 01:52:50 +0300

    [nvim] add one more hotkey to fugitive

--- dot_config/nvim/lua/n0nvme/plugins/fugitive.lua
+++ dot_config/nvim/lua/n0nvme/plugins/fugitive.lua
@@ -7,5 +7,8 @@ return {
     vim.keymap.set("n", "<leader>P", function()
       vim.cmd.Git("push")
     end, opts)
+    vim.keymap.set("n", "<leader>G", function()
+      vim.cmd.Git()
+    end, opts)
   end,
 }
