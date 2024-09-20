0e737f8dde2e5bed14dec2e8d93cd7bbb4c74c14
n0nvme <svmpl3nvm3@gmail.com>
2024-09-20 18:41:28 +0300

    [scripts] add scritps to enable daemons and add my user to required groups

---
+++ scripts/run_once_systemd.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/zsh
+systemctl enable --user ssh-agent
+systemctl enable --user avizo
+systemctl enable --user waybar
+systemctl enable --user swaync
+
---
+++ scripts/run_once_user_groups.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/zsh
+sudo usermod -aG tss n0nvme
+sudo usermod -aG video n0nvme
+sudo usermod -aG input n0nvme
