6586c7e32e24d66fffeb44fdedea9b341f518cfd
n0nvme <svmpl3nvm3@gmail.com>
2024-11-14 18:37:38 +0300

    [nvim] add golang-ci lint and stylelint

---
+++ dot_config/nvim/lua/n0nvme/plugins/lint.lua
@@ -0,0 +1,26 @@
+return {
+  "mfussenegger/nvim-lint",
+  config = function()
+    require("lint").linters_by_ft = {
+      go = { "golangcilint" },
+      css = { "stylelint" },
+    }
+    vim.api.nvim_create_user_command("LintInfo", function()
+      local filetype = vim.bo.filetype
+      local linters = require("lint").linters_by_ft[filetype]
+
+      if linters then
+        print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+      else
+        print("No linters configured for filetype: " .. filetype)
+      end
+    end, {})
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        -- try_lint without arguments runs the linters defined in `linters_by_ft`
+        -- for the current filetype
+        require("lint").try_lint()
+      end,
+    })
+  end,
+}
