4e5ddc6e5cae202dd6022de7c5db063f9522034c
n0nvme <svmpl3nvm3@gmail.com>
2024-12-19 11:02:20 +0300

    [nvim] add todo-comments plugin

--- dot_config/nvim/lua/n0nvme/plugins/init.lua
+++ dot_config/nvim/lua/n0nvme/plugins/init.lua
@@ -50,6 +50,15 @@ return {
       })
     end,
   },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
   "gleam-lang/gleam.vim",
   "dstein64/vim-startuptime",
 }
