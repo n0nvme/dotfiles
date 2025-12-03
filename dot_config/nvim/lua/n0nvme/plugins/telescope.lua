8748acfa3bff231175f52d83e7b17b6f49ed52ab
n0nvme <svmpl3nvm3@gmail.com>
2025-12-03 18:18:56 +0300

    [nvim] add ui-select extension to telescope

--- dot_config/nvim/lua/n0nvme/plugins/telescope.lua
+++ dot_config/nvim/lua/n0nvme/plugins/telescope.lua
@@ -6,9 +6,28 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
     },
+    {
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+  },
+  opts = {
+    extensions = {
+      ["ui-select"] = {
+        require("telescope.themes").get_cursor({}),
+      },
+    },
   },
   config = function()
+    require("telescope").setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_cursor({}),
+        },
+      },
+    })
     require("telescope").load_extension("fzf")
+    --require("telescope.builtin").lsp_code_actions(require("telescope.themes").get_cursor())
+    require("telescope").load_extension("ui-select")
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>fp", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
